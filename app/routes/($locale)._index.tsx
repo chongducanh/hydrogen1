a3efadb8d521a1dad19eca1eb03689d34374eb98
anhchong <anh.chong@secomm.vn>
2023-08-03 16:11:55 +0700

    fít

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -7,24 +7,54 @@ import type {
   FeaturedCollectionFragment,
   RecommendedProductsQuery,
 } from 'storefrontapi.generated';
+import {builder, BuilderComponent} from '@builder.io/react';
+builder.init('5ff9122e249a486280d63ca7d5939ca0');
+import {useEffect, useState} from 'react';
 
 export const meta: V2_MetaFunction = () => {
   return [{title: 'Hydrogen | Home'}];
 };
 
-export async function loader({context}: LoaderArgs) {
+export async function loader({context, request}: LoaderArgs) {
   const {storefront} = context;
   const {collections} = await storefront.query(FEATURED_COLLECTION_QUERY);
   const featuredCollection = collections.nodes[0];
   const recommendedProducts = storefront.query(RECOMMENDED_PRODUCTS_QUERY);
 
-  return defer({featuredCollection, recommendedProducts});
+  const page = await builder
+      .get('page-section', {
+        options: {
+          query: {
+            id: "948239e6de2c46a9bc356ef061981dd3",
+          },
+        },
+      })
+      .toPromise();
+
+  const isPreviewing = new URL(request.url).searchParams.has('builder.preview');
+  if (!page && !isPreviewing) {
+    throw new Response('Page Not Found', {
+      status: 404,
+
+      statusText:
+          "We couldn't find this page, please check your url path and if the page is published on Builder.io.",
+    });
+  }
+
+  return defer({featuredCollection, recommendedProducts, page});
 }
 
 export default function Homepage() {
   const data = useLoaderData<typeof loader>();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <div className="home">
+      {data.page && mounted && (
+          <BuilderComponent model={'page-section'} content={data.page} />
+      )}
       <FeaturedCollection collection={data.featuredCollection} />
       <RecommendedProducts products={data.recommendedProducts} />
     </div>
